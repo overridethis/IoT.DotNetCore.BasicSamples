129fd6c676272f70e1e45496d0053a5f0b815d4d
Roberto Hernandez <roberto.hernandez@infernored.com>
2020-09-25 21:01:18 -0400

    Start point

---
+++ src/IoTBasicSamples/DemoType.cs
@@ -0,0 +1,11 @@
+﻿namespace IoTBasicSamples
+{
+    enum DemoType
+    {
+        Led,    // GPIO
+        Button, // GPIO
+        Buzzer, // PWM
+        RgbLed, // DeviceBinding - PWM
+        DHT11   // DeviceBinding 
+    }
+}
---
+++ src/IoTBasicSamples/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace IoTBasicSamples
+{
+    class Program
+    {
+        private const string RunModeError = "[RUN] Must provide a valid mode to run.";
+
+        static void Main(string[] args)
+        {
+            if (!args.Any() && !string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine(RunModeError);
+                return;
+            }
+
+            if (!Enum.TryParse<DemoType>(args[0].Trim(), true, out var demoType))
+            {
+                Console.WriteLine(RunModeError);
+                return;
+            }
+
+            var tokenSource = new CancellationTokenSource();
+
+            Console.WriteLine("[RUN]: Demo");
+            Console.WriteLine($"[RUN:{demoType.ToString()}] Running");
+
+            var runTask = Task.Run(async () =>
+                {
+                    switch (demoType)
+                    {
+                        default:
+                            Console.WriteLine($"[RUN:{demoType.ToString()}] Not Implemented!");
+                            break;
+                    }
+                }, tokenSource.Token);
+
+
+            Console.WriteLine($"[RUN{demoType.ToString()}]: Press any key to exit.");
+            Console.ReadKey();
+
+            tokenSource.Cancel();
+            runTask.Wait();
+
+            Console.WriteLine("[RUN]: End");
+        }
+    }
+}
